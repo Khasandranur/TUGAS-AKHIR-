39da516f13f35be7e3682327447a737ed275dc8b
unknown <khasandranur@gmail.com>
2021-05-24 20:30:48 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+     <head>
+          <title>Praktikum DKP 2021</title>
+     <head>
+     
+    
+         
+		  Pilih Genre : <br/>
+		
+			<input type="checkbox" name="check1" value="Horror">Horror<br/>
+			<input type="checkbox" name="check2" value="Comedy">Comedy<br/>
+			<input type="checkbox" name="check3" value="Action">Action<br/>
+            <input type="checkbox" name="check4" value="Drama">Drama<br/>
+
+
+          Pilih Tahun : <br/>
+		
+			<input type="checkbox" name="check1" value="2016">2016<br/>
+			<input type="checkbox" name="check2" value="2017">2017<br/>
+			<input type="checkbox" name="check3" value="2018">2018<br/>
+            <input type="checkbox" name="check4" value="2019">2019<br/>
+
+
+            Pilih Negara : <br/>
+		
+        <input type="checkbox" name="check1" value="USA">USA<br/>
+        <input type="checkbox" name="check2" value="Korea">Korea<br/>
+        <input type="checkbox" name="check3" value="Jepang">Jepang<br/>
+        <input type="checkbox" name="check4" value="UK">UK<br/>
+			<input type="submit" value="OK"/>
+		</form>
+		  
+     </body>
+</html>
